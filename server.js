4490f5bb817003a46b27bd0064980851b9500587
Gina Im <eaim928@gmail.com>
2021-09-23 09:31:41 -0600

    connect with db attempy

--- server.js
+++ server.js
@@ -15,15 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/shrouded-ocean",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
