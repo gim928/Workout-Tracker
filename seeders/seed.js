e1595478818e73974bd809375025f7104a422a9b
Gina Im <eaim928@gmail.com>
2021-09-24 09:52:51 -0600

    added connection to seed file

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 
